ec399cbf769c926a32f77c874dfb3e2fbebaef32
Tim Rapske <rapsket@gmail.com>
2012-04-26 13:00:41 -0700

    Base Classes

---
+++ Lib/HealthismQuiz.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A00764761
+ * @version 1.0 
+ */
+public class HealthismQuiz {
+
+}
---
+++ Lib/ScalarMCQuestion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A00764761
+ * @version 1.0 
+ */
+public class ScalarMCQuestion extends MCQuestion {
+    
+    private String queryText;
+    private int queryAnswer;
+
+}

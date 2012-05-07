8ae63b20af247b58d6fb9478f89d7cfb233c2da7
Kevin Ng <kevin123567@hotmail.com>
2012-05-08 04:23:32 +0800

    No comment

---
+++ repository/projectXByKevin/src/ca/bcit/comp4900/healthydroid/BackupActivity.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp4900.healthydroid;
+
+import ca.bcit.comp4900.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BackupActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.backup);
+	}
+}
